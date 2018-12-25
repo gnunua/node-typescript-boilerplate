a3f7090fd6a1ca04f8c61e0d44987654f1af93e8
gnun <gnunua@gmail.com>
2018-12-25 19:10:11 +0400

    test3

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,13 @@ import express from 'express';
 const app = express();
 const port = 7878;
 
+let a = 9;
+a = 8;
+
+console.warn(a);
+console.log('--99-');
+console.log(process.env.PORT);
+
 app.get('/', (req, res) => res.send(''));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
