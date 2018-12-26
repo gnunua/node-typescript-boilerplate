6891e7e59fa0d9e04460c95c0f2db13f218602c5
gnun <gnunua@gmail.com>
2018-12-26 11:24:16 +0400

    test

--- dist/server.js
+++ dist/server.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
-const port = 7878;
+const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     console.log('1');
     return res.send('1');
