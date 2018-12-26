2b3d6e9e0bb618e2e13a09dc5cad02ef1f0e41b5
gnun <gnunua@gmail.com>
2018-12-26 11:22:02 +0400

    test

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const port = 7878;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   console.log('1');
@@ -11,7 +11,5 @@ app.get('/2', (req, res) => {
   console.log('2');
   return res.send('2');
 });
-
-//  "postinstall": "npm run build",
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
